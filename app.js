ef63c7f4837c24f162ccd575c3cb632348ecc776
Dinesh Mahesh Lalwani <mr.d.m.lalwani@gmail.com>
2024-01-03 13:24:29 +0530

    v3 Laying the foundation

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM  from "react-dom/client";
 
-const heading2 = React.createElement(
-  "h1",
-  { className: "greeting" },
-  "Namaste Everyone from React!!"
+const Title = () => (     // Normal JS Function
+    <h1>
+        Good Morning!!
+    </h1>
 );
-const heading3 = React.createElement(
-  "h2",
-  { className: "greeting" },
-  "Namaste 2 Everyone from React!!"
+const heading2 = (      // React element/ normal variable
+    <h1>
+        Namaste from JSX!!
+    </h1>
 );
-console.log(heading2);
+const HeaderComponent = function() {         // Functional Component
+    return(
+        <div>
+            <Title />  {/*Title()*/}
+            {heading2}
+            <h2>Namaste from functional component!!</h2>
+        </div>
+    );
+};
 // We can have only one root and one render method throughtout the app
-const container = React.createElement(
-  "div",
-  { className: "container", id: "containerID" },
-  [heading2, heading3]
-);
 const root2 = ReactDOM.createRoot(document.getElementById("root2"));
 // passing a react element into the root
-root2.render(container);
+// root2.render(heading2);
+root2.render(<HeaderComponent />);
